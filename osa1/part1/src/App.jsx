3000d59774e6add18a1ee5133646b735c93fea5a
isabel <isabel.suorsa@helsinki.fi>
2025-03-13 14:09:05 +0200

    1.4: kurssitiedot step4

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -26,27 +26,29 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1}/>
-      <Content part={part2}/>
-      <Content part={part3}/>
-      <Total parts={[part1, part2, part3]}/>
+      <Content part={parts[0]}/>
+      <Content part={parts[1]}/>
+      <Content part={parts[2]}/>
+      <Total parts={parts}/>
     </div>
   )
 }
