048466ea236aa039967c2c7f8447ec4aa6b000a4
isabel <isabel.suorsa@helsinki.fi>
2025-04-11 15:24:37 +0300

    1.8: unicafe step3

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {props.stats[0]}</p>
+      <p>Neutral {props.stats[1]}</p>
+      <p>Bad {props.stats[2]}</p>
+      <p>All {props.stats[0]+props.stats[1]+props.stats[2]}</p>
+      <p>Average {((props.stats[0]*1)+(props.stats[2]*-1))/(props.stats[0]+props.stats[1]+props.stats[2])}</p>
+      <p>Positive {100*(props.stats[0])/(props.stats[0]+props.stats[1]+props.stats[2])} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -19,13 +33,7 @@ const App = () => {
         bad
       </button>
 
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
-      <p>average {((good*1)+(bad*-1))/(good+neutral+bad)}</p>
-      <p>positive {100*(good/(good+neutral+bad))} %</p>
+      <Statistics stats={[good, neutral, bad]}/>
     </div>
   )
 }
