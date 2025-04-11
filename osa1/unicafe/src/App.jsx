4937ece793cecdb1e0f6d7274f1ee4714496ddad
isabel <isabel.suorsa@helsinki.fi>
2025-04-11 15:40:06 +0300

    1.9: unicafe step4

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
+  if (props.total === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>Statistics</h1>
-      <p>Good {props.stats[0]}</p>
-      <p>Neutral {props.stats[1]}</p>
-      <p>Bad {props.stats[2]}</p>
-      <p>All {props.stats[0]+props.stats[1]+props.stats[2]}</p>
-      <p>Average {((props.stats[0]*1)+(props.stats[2]*-1))/(props.stats[0]+props.stats[1]+props.stats[2])}</p>
-      <p>Positive {100*(props.stats[0])/(props.stats[0]+props.stats[1]+props.stats[2])} %</p>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+      <p>All {props.total}</p>
+      <p>Average {((props.good*1)+(props.bad*-1))/(props.total)}</p>
+      <p>Positive {100*(props.good)/(props.total)} %</p>
     </div>
   )
 }
@@ -19,21 +27,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
+      <button onClick={(handleGoodClick)}>good</button>
+      <button onClick={(handleNeutralClick)}>neutral</button>
+      <button onClick={(handleBadClick)}>bad</button>
 
-      <Statistics stats={[good, neutral, bad]}/>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
 }
